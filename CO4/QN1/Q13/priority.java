87aa47a44673d5e45aae31c75f2621d23a0a0836
ANSIYA-SHAJAHAN <95459632+ANSIYA-SHAJAHAN@users.noreply.github.com>
2022-06-09 15:08:43 +0530

    Add files via upload

---
+++ CO4/QN1/Q13/priority.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class priority {
+	public static void main(String args[]) {
+		PriorityQueue<String> queue = new PriorityQueue<String>();
+		queue.add("Ansiya");
+		queue.add("Shajahan");
+		System.out.println(queue);
+	}
+}

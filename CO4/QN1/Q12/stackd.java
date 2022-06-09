bceab71d8598dca807a0a36c9f3696d621ac2f19
ANSIYA-SHAJAHAN <95459632+ANSIYA-SHAJAHAN@users.noreply.github.com>
2022-06-09 15:00:35 +0530

    Add files via upload

---
+++ CO4/QN1/Q12/stackd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class stackd {
+	public static void main(String args[]) {
+		Stack<Integer> mystack = new Stack<Integer>();
+		mystack.add(20);
+		mystack.add(30);
+		mystack.add(40);
+		mystack.add(50);
+		System.out.println("Mystack : "+mystack);
+		// remove an object from the Stack when the position is passed as parameter
+		mystack.remove(1);
+		System.out.println("\nMystack after removing element at index 1 : "+mystack);
+	}
+	
+	
+}

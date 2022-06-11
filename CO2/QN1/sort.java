91045c1bd8d550a54f1fa7802a065dc5145e5f77
ANSIYA-SHAJAHAN <95459632+ANSIYA-SHAJAHAN@users.noreply.github.com>
2022-06-12 00:07:14 +0530

    Update sort.java

--- CO2/QN1/sort.java
+++ CO2/QN1/sort.java
@@ -8,7 +8,7 @@ public class sort
         Scanner scan = new Scanner(System.in);
 
         //User will be asked to enter the count of strings
-        System.out.print("Enter number of strings you would like to enter:");
+        System.out.println("Enter number of strings you would like to enter:");
         count = scan.nextInt();
 
 
